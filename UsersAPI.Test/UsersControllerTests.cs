161e32cec263bd0a25eaf2f21dcbe89b708fb009
Gabriel Armendariz <gabrielkdc@hotmail.com>
2024-03-27 09:03:03 -0600

    Added Integration tests project

---
+++ UsersAPI.Test/UsersControllerTests.cs
@@ -0,0 +1,81 @@
+using System.Net;
+using System.Net.Http.Json;
+using UsersAPI.Models;
+
+namespace UsersAPI.Test;
+
+public class UsersControllerTests
+{
+    [Fact]
+    public async Task RegisterUser_ShouldReturnOk_WhenAllFieldsAreFilled()
+    {
+        // Arrange  -- Prepare the test data
+
+        var r = new Random(DateTime.Now.Millisecond);
+
+        var newUser = new User
+        {
+            Name = "TestUser",
+            Username = $"testuser{r.NextDouble()}",
+            Password = "123456"
+        };
+
+        var httpClient = new HttpClient();
+
+        // Act -- Call the method to be tested
+        var result = await httpClient.PostAsJsonAsync("https://localhost:7266/Users", newUser);
+        
+        // Assert -- Validate the result
+        
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+    }
+    
+    [Fact]
+    public async Task RegisterUser_ShouldFailWithPasswordEmtpy_WhenPasswordIsNull()
+    {
+        // Arrange  -- Prepare the test data
+
+        var newUser = new User
+        {
+            Name = "TestUser",
+            Username = "testuser"
+        };
+
+        var httpClient = new HttpClient();
+
+        // Act -- Call the method to be tested
+        var result = await httpClient.PostAsJsonAsync("https://localhost:7266/Users", newUser);
+        
+        // Assert -- Validate the result
+        
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+
+    }
+    
+    [Fact]
+    public async Task RegisterUser_ShouldFail_WhenPasswordIsLessThan6Characters()
+    {
+        // Arrange  -- Prepare the test data
+
+        var newUser = new User
+        {
+            Name = "TestUser",
+            Username = "testuser",
+            Password = "12345"
+        };
+
+        var httpClient = new HttpClient();
+
+        // Act -- Call the method to be tested
+        var result = await httpClient.PostAsJsonAsync("https://localhost:7266/Users", newUser);
+        
+        // Assert -- Validate the result
+        
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+
+    }
+    
+
+
+}
