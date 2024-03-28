6cd3292a11da8ff6390db63e312566f792eac809
Yahir <yahir21077@gmail.com>
2024-03-28 11:02:55 -0700

    Return Not found when the user doesn't exist
    new file for the test of GetUserDetails Method in where a new test was added for check if the user exists or not

---
+++ UsersAPI.Test/GetUserDetailsTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using UsersAPI.Models;
+
+namespace UsersAPI.Test;
+
+public class GetUserDetailsTests : IClassFixture<WebApplicationFactory<IApiMarker>>, IAsyncLifetime
+{
+    private readonly HttpClient httpClient;
+    readonly List<int> createdUsersIds = new List<int>();
+    public GetUserDetailsTests(WebApplicationFactory<IApiMarker> webApplicationFactory)
+    {
+        httpClient = webApplicationFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task ShouldReturnNotfoun_WhenTheUserDoesNotExists()
+    {
+        // Arrange -- prepare the test data
+
+        var newUser = new User
+        {
+            Name="Nombre",
+            Password="1234567",
+            Username="Username2"
+        };
+        await httpClient.PostAsJsonAsync("Users", newUser);
+        var UserId = newUser.Id;
+        httpClient.DeleteAsync($"Users/{UserId}");
+
+        // Act call the method to be tested
+
+        var result = await httpClient.GetAsync($"/Users/{UserId}");
+
+        // Asert -- Validate the Result
+
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+
+    }
+
+    public Task InitializeAsync()
+    {    
+        return Task.CompletedTask;
+    }
+
+    public async Task DisposeAsync()
+    {
+        var deleteTasks = new List<Task>();
+        foreach (var userId in createdUsersIds)
+        {
+            deleteTasks.Add(httpClient.DeleteAsync($"Users/{userId}"));
+        }
+        await Task.WhenAll(deleteTasks);
+        httpClient.Dispose();
+    }
+}
+
